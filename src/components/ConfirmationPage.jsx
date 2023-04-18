3341bf43456e69f36faa0b1aef677755d95a557d
d14mittal <44315483+d14mittal@users.noreply.github.com>
2023-04-18 17:09:07 -0400

    Add a route in AppRoutes component to render this component at the route: “/order-confirmation/:id”

--- src/components/ConfirmationPage.jsx
+++ src/components/ConfirmationPage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-function ConfirmationPage() {
+function ConfirmationPage({ match }) {
+  const orderId = match.params.id;
   return (
     <div>
       <h1>Confirmation Page</h1>
-      <p>Thank you for your order!</p>
+      <p>Thank you for your order #{orderId}!</p>
     </div>
   );
 }
