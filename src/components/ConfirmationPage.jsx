b6f12964dfb7dcedaadf6c5dab5cb698f30c97f4
d14mittal <44315483+d14mittal@users.noreply.github.com>
2023-04-18 17:20:21 -0400

    This component should connect to the server and retrieve an order based on the id (the endpoint lives at “/api/orders/<id>”

--- src/components/ConfirmationPage.jsx
+++ src/components/ConfirmationPage.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function ConfirmationPage({ match }) {
+  const [order, setOrder] = useState(null);
   const orderId = match.params.id;
+
+  useEffect(() => {
+    fetch(`/api/orders/${orderId}`)
+      .then((response) => response.json())
+      .then((data) => setOrder(data));
+  }, [orderId]);
+
+  if (!order) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <h1>Confirmation Page</h1>
       <p>Thank you for your order #{orderId}!</p>
+      <p>Order details:</p>
+      <ul>
+        <li>Order ID: {order.id}</li>
+        <li>Order Date: {order.date}</li>
+        <li>Order Total: {order.total}</li>
+      </ul>
     </div>
   );
 }
