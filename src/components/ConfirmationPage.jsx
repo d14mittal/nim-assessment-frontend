401bf73b4a9c394b60624db0e210ca59b3c064b3
d14mittal <44315483+d14mittal@users.noreply.github.com>
2023-04-18 16:29:02 -0400

    Create a new React component, called ConfirmationPage

---
+++ src/components/ConfirmationPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ConfirmationPage() {
+  return (
+    <div>
+      <h1>Confirmation Page</h1>
+      <p>Thank you for your order!</p>
+    </div>
+  );
+}
+
+export default ConfirmationPage;
